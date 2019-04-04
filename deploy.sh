b8ecdcfbd366c1b92132d6e135277b72fae2071b
Vicente Chavez <vchavez92780@gmail.com>
2019-04-04 15:36:26 -0400

    Added missing file

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ cp -r "$_cwd/src" "$_targetRoot/$_rootDirName/src"
 cp "$_cwd/.env.example" "$_targetRoot/$_rootDirName/.env"
 cp "$_cwd/app.py" "$_targetRoot/$_rootDirName/app.py"
 cp "$_cwd/requirements.txt" "$_targetRoot/$_rootDirName/requirements.txt"
+cp "$_cwd/test_data.json" "$_targetRoot/$_rootDirName/test_data.json"
 find "$_targetRoot/$_rootDirName" | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 cd "$_targetRoot"
 zip -r "$_targetRoot/$_rootDirName.zip" ./${_rootDirName}/*
--- src/config.py
+++ src/config.py
@@ -9,15 +9,16 @@ dotenv_path = os.path.join(ROOT_DIR, '.env')
 load_dotenv(dotenv_path)
 
 # Set the settings
+
 config = {
 	'DB_SERVER': os.getenv('DB_HOST'),
 	'DB_NAME': os.getenv('DB_NAME'),
 	'DB_USER': os.getenv('DB_USER'),
 	'DB_PASSWORD': os.getenv('DB_PASSWORD'),
 	'DB_DRIVER': os.getenv('DB_DRIVER'),
-	'DB_TRUSTED_CONNECTION': os.getenv('DB_TRUSTED_CONNECTION') == 1,
+	'DB_TRUSTED_CONNECTION': os.getenv('DB_TRUSTED_CONNECTION') == '1',
 	'ROOT_DIR': ROOT_DIR,
-	'IS_PRODUCTION': os.getenv('PRODUCTION') == 1,
+	'IS_PRODUCTION': os.getenv('PRODUCTION') == '1',
 	'TELECOM_REP_ENDPOINT': 'http://lionwebapiuat-fr.ultimatemedical.edu/api/v2/Telecom/GetAllUsers'
 }
 

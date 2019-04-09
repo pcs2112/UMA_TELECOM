6f68ee4bd4af4da2f628043f6b86bee3b25bfdcb
Vincent Chavez <vchavez92780@gmail.com>
2019-04-09 11:35:27 -0400

    Added back the sql server package

--- src/commands/process_reps.py
+++ src/commands/process_reps.py
@@ -1,12 +1,14 @@
-from py_utils.mssql_db import init_db
+from src.mssql_db import init_db, close
 from src.config import get_config
-from src.rep_helper import fetch_api_rep_data, save_rep, save_rep_role, save_rep_skill, save_rep_workgroup
+from src.rep_helper import (
+	fetch_api_rep_data, start_process, stop_process, save_rep, save_rep_role, save_rep_skill, save_rep_workgroup
+)
 from src.utils import format_number
 
 
 def process_reps():
 	config = get_config()
-	
+
 	db_config = {
 		'DB_SERVER': config['DB_SERVER'],
 		'DB_NAME': config['DB_NAME'],
@@ -17,30 +19,40 @@ def process_reps():
 	}
 
 	init_db(db_config)
-	
+
 	total_processed = 0
-	
+
 	print("Processing ...")
-	
+
+	master_load_id = start_process()
+	if not master_load_id:
+		print("Processing halted. Master load id not returned.")
+		exit()
+
 	data = fetch_api_rep_data()
-	
+
 	total_count = len(data['dataList'])
-	
+
 	if total_count > 0:
 		for rep in data['dataList']:
-			rep_id = save_rep(rep)
-			
+			rep_id = save_rep(rep, master_load_id)
+
 			if rep_id:
 				for role in rep['roles']:
-					save_rep_role(rep_id, role)
-					
+					save_rep_role(rep_id, role, master_load_id)
+
 				for skill in rep['skills']:
-					save_rep_skill(rep_id, skill)
-					
+					save_rep_skill(rep_id, skill, master_load_id)
+
 				for workgroup in rep['workgroups']:
-					save_rep_workgroup(rep_id, workgroup)
-					
+					save_rep_workgroup(rep_id, workgroup, master_load_id)
+
 				total_processed += 1
-	
-	print(f"TOTAL: {format_number(total_count)}")
-	print(f"TOTAL REPS PROCESSED: {format_number(total_processed)}")
+
+	summary = stop_process(master_load_id)
+
+	print(f"TOTAL ROWS PROCESSED: {format_number(summary['return_value'])}")
+	print(f"TOTAL NEW REPS: {format_number(summary['new_rep_count'])}")
+	print(f"TOTAL ROWS REMOVED: {format_number(summary['removed_count'])}")
+
+	close()
--- src/rep_helper.py
+++ src/rep_helper.py
@@ -2,8 +2,8 @@ import os
 import json
 import requests
 from .config import get_config
-from py_utils.mssql_db import execute_sp, get_sp_first_result_set
-from src.utils import format_date
+from .mssql_db import execute_sp, get_sp_first_result_set
+from .utils import format_date
 
 
 config = get_config()
@@ -13,7 +13,7 @@ def _get_custom_attributes(custom_attributes):
 	custom_attrs = json.dumps(custom_attributes)
 	if custom_attrs == '{}':
 		custom_attrs = None
-		
+
 	return custom_attrs
 
 
@@ -21,7 +21,7 @@ def fetch_api_rep_data():
 	""" Return the Telecom API rep json data. """
 	if config['IS_PRODUCTION']:
 		return fetch_api_prod_rep_data()
-	
+
 	return fetch_api_test_rep_data()
 
 
@@ -30,23 +30,56 @@ def fetch_api_test_rep_data():
 	file_path = os.path.join(config['ROOT_DIR'], 'test_data.json')
 	if os.path.exists(file_path) is False:
 		raise FileExistsError(f"The test data file was not found.")
-	
+
 	with open(file_path) as fp:
 		contents = fp.read()
-	
+
 	return json.loads(contents)
-	
+
 
 def fetch_api_prod_rep_data():
 	""" Returns JSON payload containing the production data. """
 	result = requests.get(config['TELECOM_REP_ENDPOINT'])
 	if result.status_code != 200:
 		result.raise_for_status()
-	
+
 	return result.json()
 
 
-def save_rep(rep):
+def start_process():
+	""" Calls the stored procedure to start the process. """
+	results = execute_sp(
+		'UMA_TELECOM.PROCESS_API_DATA',
+		{
+			'MESSAGE': 'START'
+		}
+	)
+
+	result = get_sp_first_result_set(results)
+	if not result:
+		return False
+
+	return result['return_value']
+
+
+def stop_process(master_load_id):
+	""" Calls the stored procedure to stop the process. """
+	results = execute_sp(
+		'UMA_TELECOM.PROCESS_API_DATA',
+		{
+			'MESSAGE': 'FINISHED',
+			'LOAD_HIST_PKID_IN': master_load_id
+		}
+	)
+
+	result = get_sp_first_result_set(results)
+	if not result:
+		return False
+
+	return result
+
+
+def save_rep(rep, master_load_id):
 	""" Saves the rep data. """
 	results = execute_sp(
 		'UMA_TELECOM.SAVE_D_REP',
@@ -62,18 +95,19 @@ def save_rep(rep):
 			'REP_outboundANI': rep['outboundANI'],
 			'REP_id_LIST': rep['id'],
 			'REP_customAttributes': _get_custom_attributes(rep['customAttributes']),
-			'REP_dateAdded': format_date(parse_date(rep['dateAdded']))
+			'REP_dateAdded': format_date(parse_date(rep['dateAdded'])),
+			'LOAD_HISTORY_PKID': master_load_id
 		}
 	)
-	
+
 	result = get_sp_first_result_set(results)
 	if not result:
 		return False
-	
+
 	return result['rep_id']
 
 
-def save_rep_skill(rep_id, rep_skill):
+def save_rep_skill(rep_id, rep_skill, master_load_id):
 	""" Saves the rep skill. """
 	results = execute_sp(
 		'UMA_TELECOM.SAVE_D_REP_SKILL',
@@ -84,14 +118,15 @@ def save_rep_skill(rep_id, rep_skill):
 			'REP_SKILL_proficiency': rep_skill['proficiency'],
 			'REP_SKILL_desireToUse': rep_skill['desireToUse'],
 			'REP_SKILL_id_ALTERNATE': rep_skill['id'],
-			'REP_SKILL_dateAdded': format_date(parse_date(rep_skill['dateAdded']))
+			'REP_SKILL_dateAdded': format_date(parse_date(rep_skill['dateAdded'])),
+			'LOAD_HISTORY_PKID': master_load_id
 		}
 	)
-	
+
 	return get_sp_first_result_set(results)
 
 
-def save_rep_role(rep_id, rep_role):
+def save_rep_role(rep_id, rep_role, master_load_id):
 	""" Saves the rep role. """
 	results = execute_sp(
 		'UMA_TELECOM.SAVE_D_REP_ROLE',
@@ -101,14 +136,15 @@ def save_rep_role(rep_id, rep_role):
 			'REP_ROLE_roleId': rep_role['roleId'],
 			'REP_ROLE_name': rep_role['name'],
 			'REP_ROLE_id_ALTERNATE': rep_role['id'],
-			'REP_ROLE_dateAdded': format_date(parse_date(rep_role['dateAdded']))
+			'REP_ROLE_dateAdded': format_date(parse_date(rep_role['dateAdded'])),
+			'LOAD_HISTORY_PKID': master_load_id
 		}
 	)
-	
+
 	return get_sp_first_result_set(results)
 
 
-def save_rep_workgroup(rep_id, rep_workgroup):
+def save_rep_workgroup(rep_id, rep_workgroup, master_load_id):
 	""" Saves the rep workgroup. """
 	results = execute_sp(
 		'UMA_TELECOM.SAVE_D_REP_WORKGROUP',
@@ -118,10 +154,11 @@ def save_rep_workgroup(rep_id, rep_workgroup):
 			'REP_WORKGROUP_Name': rep_workgroup['name'],
 			'REP_WORKGROUP_customAttributes': _get_custom_attributes(rep_workgroup['customAttributes']),
 			'REP_WORKGROUP_id_ALTERNATE': rep_workgroup['id'],
-			'REP_WORKGROUP_dateAdded': format_date(parse_date(rep_workgroup['dateAdded']))
+			'REP_WORKGROUP_dateAdded': format_date(parse_date(rep_workgroup['dateAdded'])),
+			'LOAD_HISTORY_PKID': master_load_id
 		}
 	)
-	
+
 	return get_sp_first_result_set(results)
 
 
